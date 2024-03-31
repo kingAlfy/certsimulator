5f49de06ff42399456ff7277c82f143a11f5afb8
kingAlfy <al2367.arg@gmail.com>
2024-03-31 20:27:14 +0200

    feat(ui): add divider between menu and main

--- frontend/src/app/(app)/Navigation.js
+++ frontend/src/app/(app)/Navigation.js
@@ -27,7 +27,7 @@ const Navigation = ({ user }) => {
                             variant="circular"
                             size="sm"
                             alt={user.name}
-                            className="border border-gray-900 p-0.5"
+                            className="border border-gray-900"
                             /* TODO: Do changes in avatar */
                             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
                         />
