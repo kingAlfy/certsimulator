9d8b371158bcaa78324fd31b4960793961add4bc
kingAlfy <al2367.arg@gmail.com>
2024-04-24 13:32:48 +0200

    feat(api): add routes to api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,16 +4,25 @@ use App\Http\Controllers\ExamController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Response;
+
+// TODO: Securizar con middleware todos los endpoint
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
-Route::get('/exams', [QuestionController::class, 'index']);
-Route::get('/exams/{examId}', [QuestionController::class, 'indexById']);
+Route::get('/exams', [ExamController::class, 'index']);
+
+Route::get('/exams/{examId}', [ExamController::class, 'show']);
+
 Route::post('/exams', [ExamController::class, 'store']);
+
+Route::delete('/exams', [ExamController::class, 'destroy']);
 
 
 Route::get('/exams/{examId}/questions', [QuestionController::class, 'indexByExam']);
+
+Route::get('/exams/{examId}/{topicId}/questions', [QuestionController::class, 'indexByExamAndTopic']);
+
+Route::get('/exams/{examId}/{topicId}/{questionNumber}', [QuestionController::class, 'indexByExamAndTopic']);
